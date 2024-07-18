1758e66f473eb5ee162c591969181387d9ac4c86
Ravikrishna <100361466+Ravikrishna25@users.noreply.github.com>
2024-07-18 08:04:03 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,17 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: postgres,
+        password: casemitra_v1,
+        database: casemitra,
+        host: casemitrards.czokwaoaczpc.ap-south-1.rds.amazonaws.com,
+        dialect: 'postgres',
+    },
+    production: {
+        username: postgres,
+        password: casemitra_v1,
+        database: casemitra,
+        host: casemitrards.czokwaoaczpc.ap-south-1.rds.amazonaws.com,
         dialect: 'postgres',
     },
     // Add similar objects for test and production environments
