2730181dc3ff247c1b921d86d2dcfb88eb05ca0f
Ravikrishna <100361466+Ravikrishna25@users.noreply.github.com>
2024-07-18 08:04:29 +0530

    Update authenticateJWT.js

--- server/middleware/authenticateJWT.js
+++ server/middleware/authenticateJWT.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET;
+const secret = K7mC46ByU6d8n5zfG4EN3Adjxuin9eNlakK0MhtZ3diw4zRl9MwbBwrey1jF7k4P;
 
 const authenticateJWT = (req, res, next) => {
     const token = req.header('Authorization');
